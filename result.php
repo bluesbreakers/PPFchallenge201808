e275f69eead149bb525eeee225dde520710d3320
ozaki makoto <ozaki@ppfpart.co.jp>
2018-08-30 23:50:14 +0900

    20180830

--- result.php
+++ result.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>入力フォーム</title>
+<title>結果画面</title>
 </head>
 <body>
 <h1>御神籤<br></h1>
@@ -14,13 +14,13 @@ echo $comment;
 echo " 殿の本日の運勢は...<br/>";
 echo "<br/>";
 $arr = array("「めっちゃ大吉」","「やや大吉」","「それなりに大吉」",
-"「だいたい中吉」","「どっちかといえば中吉」","「けっこう小吉」","「まぁまぁ吉」","「やっぱり凶」");
+"「だいたい中吉」","「どっちかといえば中吉」","「けっこう小吉」","「まぁまぁ吉」","「かろうじて吉」","「やっぱり凶」");
 echo $arr[array_rand($arr)];
 echo "<br/>";
 echo "<br/>";
 echo "にて御座候！！！<br/>";
 echo "<br/>";
-echo "<a href='form.html'>再試行</a>";
+echo "<a href='form.html'>帰還</a>";
 ?>
 </form>
 </body>
