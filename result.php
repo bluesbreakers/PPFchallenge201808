38aa4764202cebabbe2407676e74f2daf072830c
ozaki makoto <ozaki@ppfpart.co.jp>
2018-08-30 23:31:07 +0900

    20180830

--- result.php
+++ result.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,15 +6,22 @@
 <title>入力フォーム</title>
 </head>
 <body>
-<h1>たばさメーカー<br></h1>
+<h1>御神籤<br></h1>
 <?php
 if(isset($_GET['comment'])){
 $comment = $_GET['comment'];
 echo $comment;
 }
-echo "たばさ！<br/>";
+echo " 殿の本日の運勢は...<br/>";
 echo "<br/>";
-echo "<a href='form.php'>診断へ戻る</a>";
+$arr = array("「めっちゃ大吉」","「やや大吉」","「それなりに大吉」",
+"「だいたい中吉」","「どっちかといえば中吉」","「けっこう小吉」","「まぁまぁ吉」","「やっぱり凶」");
+echo $arr[array_rand($arr)];
+echo "<br/>";
+echo "<br/>";
+echo "にて御座候！！！<br/>";
+echo "<br/>";
+echo "<a href='form.html'>再試行</a>";
 ?>
 
 </form>
